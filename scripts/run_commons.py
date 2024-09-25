81d2f3733db1fc51bc24cb42330b2760ee018a80
Ilia Malakhov <ilya.malakhov4@gmail.com>
2024-09-25 09:50:19 +0200

    Better run-tests

--- scripts/run_commons.py
+++ scripts/run_commons.py
@@ -4,26 +4,26 @@ import subprocess
 from os import path
 
 
-def get_test_command(test_name):
+def get_run_command(target_name):
     command = [
         "./gradlew",  "--no-daemon",
-        ":test", "--tests", test_name
+        ":test", "--tests", target_name
     ]
     return command
 
-def run_test(test_name, home_dir, logs_dir, jacoco_exec_dir):
+def run_target(target_name, home_dir, logs_dir, jacoco_exec_dir):
     subprocess.run("pwd")
-    logging.debug("Running test" + test_name)
+    logging.debug("Running target" + target_name)
 
-    command = get_test_command(test_name)
+    command = get_run_command(target_name)
     logging.debug("command: " + str(command))
     my_env = os.environ.copy()
     my_env["JAZZER_FUZZ"] = "1"
-    my_env["JAZZER_COVERAGE_DUMP"] = str(path.join(jacoco_exec_dir, test_name + ".exec"))
+    my_env["JAZZER_COVERAGE_DUMP"] = str(path.join(jacoco_exec_dir, target_name + ".exec"))
 
     # timestamp = datetime.datetime.now().strftime("%Y-%m-%d--%H-%M-%S")
-    stdout_file = open(path.join(logs_dir, test_name), "w")
-    stderr_file = open(path.join(logs_dir, test_name) + ".err", "w")
+    stdout_file = open(path.join(logs_dir, target_name), "w")
+    stderr_file = open(path.join(logs_dir, target_name) + ".err", "w")
     subprocess.run(command, env=my_env, stderr=stderr_file, stdout=stdout_file, cwd=home_dir)
     stdout_file.close()
     stderr_file.close()
