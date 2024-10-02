4212446ef91f50d60ebdf855dd6942d25790b46a
Ilia Malakhov <ilya.malakhov4@gmail.com>
2024-10-02 15:05:40 +0200

    reproducer fix

--- kotlinx.collections.immutable/src/test/kotlin/org/plan/research/Reproduce.kt
+++ kotlinx.collections.immutable/src/test/kotlin/org/plan/research/Reproduce.kt
@@ -21,21 +21,4 @@ class Reproduce {
         assertEquals(set2, builder.build().toSet())
         assertEquals(set2, builder.build())
     }
-
-    @Test
-    fun persistentOrderedSetNotEqualsBuildera(){
-        val set1 = persistentSetOf(-1, 2, 0, 3)
-        val builder = set1.builder()
-
-        assertEquals(set1, builder)
-        assertEquals(set1, builder.build())
-        assertEquals(set1, builder.build().toSet())
-
-
-        val set2 = set1.remove(0)
-        builder.remove(0)
-
-        assertEquals(set2, builder.build().toSet())
-        assertEquals(set2, builder.build())
-    }
 }
