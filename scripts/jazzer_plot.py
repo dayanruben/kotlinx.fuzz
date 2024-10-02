be134dfb60c223c06ab25f9fc1f80fc4ff3f71c7
Ilia Malakhov <ilya.malakhov4@gmail.com>
2024-10-02 15:51:49 +0200

    fix

--- scripts/jazzer_plot.py
+++ scripts/jazzer_plot.py
@@ -21,8 +21,8 @@ class CrashEntry:
     inputPath: str
 
 
-# 944   NEW    cov: 57 ft: 58 corp: 10/20b lim: 6 exec/s: 0 rss: 718Mb L: 2/3 MS: 2 ChangeBinInt-Custom-
-# 957   REDUCE cov: 63 ft: 64 corp: 11/26b lim: 6 exec/s: 0 rss: 718Mb L: 6/6 MS: 6 CMP-Custom-CrossOver-Custom-ShuffleBytes-Custom- DE: "\377\377\377\000"-
+# 944	NEW    cov: 57 ft: 58 corp: 10/20b lim: 6 exec/s: 0 rss: 718Mb L: 2/3 MS: 2 ChangeBinInt-Custom-
+# 957	REDUCE cov: 63 ft: 64 corp: 11/26b lim: 6 exec/s: 0 rss: 718Mb L: 6/6 MS: 6 CMP-Custom-CrossOver-Custom-ShuffleBytes-Custom- DE: "\377\377\377\000"-
 
 def libfuzzer_output_to_csv(filename, duration) -> (str, str):
     lines = [LibfuzzerLogEntry(execNr=0, cov=0, ft=0, crashes=0)]
@@ -69,4 +69,3 @@ def parse_duration(duration):
 
     parts = match.groupdict(default='0')
     return int(parts['hours']) * 3600 + int(parts['minutes']) * 60 + int(parts['seconds'])
-
