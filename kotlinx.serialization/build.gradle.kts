10830af908c773cf55c5766da34d46553dbabe5c
Azat Abdullin <azat.aam@gmail.com>
2024-10-08 14:38:45 +0200

    Merge with main: switch to new Gradle project structure

--- kotlinx.serialization/build.gradle.kts
+++ kotlinx.serialization/build.gradle.kts
@@ -1,41 +1,9 @@
 plugins {
-    kotlin("jvm") version "2.0.20"
-    kotlin("plugin.serialization") version "2.0.20"
-}
-
-repositories {
-    mavenCentral()
+    id("org.plan.research.kotlinx-fuzz-submodule")
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.2")
-    testImplementation("com.code-intelligence:jazzer-junit:0.22.1")
-    testImplementation("com.code-intelligence:jazzer-api:0.22.1")
-
-
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-cbor:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-properties:1.7.3")
 }
-
-kotlin {
-    jvmToolchain(17)
-}
-
-tasks.named<Test>("test") {
-    // Use JUnit Platform for unit tests.
-    useJUnitPlatform()
-    testLogging.showStandardStreams = true // adds more info to logs
-
-    environment(mapOf("JAZZER_FUZZ" to "1"))
-
-    maxHeapSize = "${1024 * 4}m"
-    jvmArgs("-Xss1g")
-}
-
-task("copyDependencies", Copy::class) {
-    from(configurations.runtimeClasspath).into("${project.layout.buildDirectory.get()}/dependencies")
-}
-
-tasks.getByName("test").dependsOn("copyDependencies")
