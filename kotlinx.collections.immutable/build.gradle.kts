192b6d1de1b03df47a5dfb06282f83d9df65e5f1
Ilia Malakhov <ilya.malakhov4@gmail.com>
2024-10-08 10:38:43 +0200

    disabled jacoco plugin

--- kotlinx.collections.immutable/build.gradle.kts
+++ kotlinx.collections.immutable/build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
     kotlin("jvm") version "2.0.10"
-//    jacoco
 }
 
 repositories {
     mavenCentral()
     mavenLocal()
 }
+
 
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.2")
@@ -25,12 +25,8 @@ tasks.test {
     useJUnitPlatform()
     testLogging.showStandardStreams = true
     maxHeapSize = "${1024 * 4}m"
-//    val jacocoDest = properties["jacocoDest"]
-//    configure<JacocoTaskExtension> {
-//        includes = listOf("kotlinx.collections.immutable.**")
-//        destinationFile = layout.buildDirectory.file("jacoco/$jacocoDest.exec").get().asFile
-//    }
 }
+
 
 kotlin {
     jvmToolchain(17)
