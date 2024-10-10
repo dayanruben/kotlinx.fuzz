e61ea49afa1030b141b1633743eb4b4be4ff88a3
Ilia Malakhov <ilya.malakhov4@gmail.com>
2024-10-10 07:04:09 +0200

    Used our plugin

--- kotlinx.html/build.gradle.kts
+++ kotlinx.html/build.gradle.kts
@@ -1,18 +1,9 @@
 plugins {
-    kotlin("jvm") version "2.0.10"
+    id("org.plan.research.kotlinx-fuzz-submodule")
 }
 
-repositories {
-    mavenCentral()
-}
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.2")
-
-    val jazzerVersion = "0.21.1"
-    testImplementation("com.code-intelligence:jazzer-api:$jazzerVersion")
-    testImplementation("com.code-intelligence:jazzer-junit:$jazzerVersion")
-
     testImplementation(kotlin("reflect"))
     implementation("org.reflections:reflections:0.10.2")
 
@@ -23,14 +14,4 @@ dependencies {
     val kotlinxHtmlVersion = "0.11.0"
     implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:$kotlinxHtmlVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-html:$kotlinxHtmlVersion")
-}
-
-tasks.test {
-    useJUnitPlatform()
-    testLogging.showStandardStreams = true
-    maxHeapSize = "${1024 * 4}m"
-}
-
-kotlin {
-    jvmToolchain(17)
 }
