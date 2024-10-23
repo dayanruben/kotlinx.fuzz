0ca4225b86005b8118dd1ad8095f50a30edd173d
Denis Lochmelis <dlochmelis@gmail.com>
2024-10-24 01:35:20 +0200

    reupload tests + check that they work with scripts!

--- kotlinx.cli/build.gradle.kts
+++ kotlinx.cli/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("org.plan.research.kotlinx-fuzz-submodule")
-    kotlin("plugin.serialization") version "2.0.20"
 }
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.6")
+    implementation(kotlin("reflect"))
 }
