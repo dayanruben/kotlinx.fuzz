c9fcad900615f8a5d84b05e473ac8ed2e0bb5b39
Ilia Malakhov <ilya.malakhov4@gmail.com>
2024-10-31 09:53:17 +0100

    Revert "Recursion depth limit, constants"
    This reverts commit 4fb081b4f3e05993e45ad2f79de4b9894c9dc38a.

--- kotlinx.html/src/test/kotlin/org/plan/research/Constants.kt
+++ kotlinx.html/src/test/kotlin/org/plan/research/Constants.kt
@@ -1,8 +1,5 @@
 package org.plan.research
 
 object Constants {
-    const val MAX_DURATION = "10m" // TODO()
-    const val MIN_METHODS = 1
-    const val MAX_METHODS = 7
-    const val MAX_DEPTH = 7
+    const val MAX_DURATION = TODO()
 }
