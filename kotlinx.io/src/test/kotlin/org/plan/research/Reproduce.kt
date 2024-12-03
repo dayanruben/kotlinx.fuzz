d49937e6455cca725d7167d743ceeb32850cae5d
Ilia Malakhov <ilya.malakhov4@gmail.com>
2024-12-03 10:43:54 +0100

    Report and reproducers

--- kotlinx.io/src/test/kotlin/org/plan/research/Reproduce.kt
+++ kotlinx.io/src/test/kotlin/org/plan/research/Reproduce.kt
@@ -1,32 +1,34 @@
 package org.plan.research
 
-import kotlinx.io.Buffer
-import kotlinx.io.asInputStream
+import kotlinx.io.*
 import kotlinx.io.bytestring.ByteString
-import kotlinx.io.indexOf
-import kotlinx.io.write
 import org.junit.jupiter.api.assertThrows
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 object Reproduce {
     @Test
-    fun bufferIndexOf() {
-        val test = Buffer()
-        test.writeByte(0)
-        val copy = test.copy()
+    fun `Bufer$indexOf and Source$indexOf inconsistency`() {
+        val buf = Buffer()
+        val bs = ByteString(0, 0)
+        val idx = -1L
+        assertEquals(-1, buf.indexOf(bs, idx))
+        assertEquals(-1, (buf as Source).indexOf(bs, idx))
+        //  inconsistency with Source.indexOf, it fails when startIndex = -1
+    }
 
-        assertThrows<Throwable> { test.write(Buffer().asInputStream(), 1) }
-        assertThrows<Throwable> { copy.write(Buffer().asInputStream(), 1) }
+    @Test
+    fun `Bufer$indexOf and Buffer$copy works strangely`() {
+        val origBuf = Buffer()
+        origBuf.writeByte(0)
+        val copyBuf = origBuf.copy()
+
+        assertThrows<Throwable> { origBuf.write(Buffer().asInputStream(), 1) }
+        assertThrows<Throwable> { copyBuf.write(Buffer().asInputStream(), 1) }
 
         val bs = ByteString(0, 0)
         val idx = -1L
-        assertEquals(-1, test.indexOf(bs, idx))
-        assertEquals(-1, copy.indexOf(bs, idx)) // fails, indexOf == 0
-        /*
-        inconsistency with Source.indexOf, it fails when startIndex = -1
-
-        otherwise this is probably fine, because `indexOf` doesn't suppose to check buffer boundaries
-         */
+        assertEquals(-1, origBuf.indexOf(bs, idx))
+        assertEquals(-1, copyBuf.indexOf(bs, idx)) // fails, indexOf == 0
     }
 }
