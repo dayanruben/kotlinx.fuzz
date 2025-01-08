169fac50fa795d49082cfc0ed4070085a8210d7b
FerrumBrain <lemur1416@gmail.com>
2025-01-08 23:35:48 +0100

    removed KFuzzEngine

--- kotlinx.fuzz.api/src/main/kotlin/kotlinx/fuzz/KFuzzEngine.kt
+++
@@ -1,9 +0,0 @@
-package kotlinx.fuzz
-
-import java.io.File
-
-interface KFuzzEngine {
-    fun initialise(classpath: String, pathToSrc: Set<File>, settings: Map<String, String>)
-    fun runTarget(target: String)
-    fun exportStatistics(): Map<String, Any>
-}
