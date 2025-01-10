e2e9dd7abd51cc659eb13386fd2dc92d04dbf473
Azat Abdullin <azat.aam@gmail.com>
2025-01-10 12:21:05 +0100

    add diktat

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,20 @@
+import com.saveourtool.diktat.plugin.gradle.DiktatExtension
+import com.saveourtool.diktat.plugin.gradle.DiktatGradlePlugin
+
 plugins {
     id("org.plan.research.kotlinx-fuzz-module")
+    id("com.saveourtool.diktat") version "2.0.0" apply false
+}
+
+allprojects {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+    }
+    apply<DiktatGradlePlugin>()
+    configure<DiktatExtension> {
+        diktatConfigFile = rootProject.file("diktat-analysis.yml")
+        inputs { include("src/**/*.kt") }
+        debug = true
+    }
 }
