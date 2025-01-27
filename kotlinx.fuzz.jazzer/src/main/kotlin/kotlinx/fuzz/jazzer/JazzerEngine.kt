3243a7e202c0b4a55d97fe780ce5b23627a553ec
Ilia Malakhov <ilya.malakhov4@gmail.com>
2025-01-27 14:17:13 +0100

    `JazzerEngine` spawn new process for each target

--- kotlinx.fuzz.jazzer/src/main/kotlin/kotlinx/fuzz/jazzer/JazzerEngine.kt
+++ kotlinx.fuzz.jazzer/src/main/kotlin/kotlinx/fuzz/jazzer/JazzerEngine.kt
@@ -1,60 +1,42 @@
 package kotlinx.fuzz.jazzer
 
-import com.code_intelligence.jazzer.agent.AgentInstaller
-import com.code_intelligence.jazzer.driver.FuzzTargetHolder
-import com.code_intelligence.jazzer.driver.FuzzTargetRunner
-import com.code_intelligence.jazzer.driver.LifecycleMethodsInvoker
-import com.code_intelligence.jazzer.driver.Opt
-import com.code_intelligence.jazzer.utils.Log
 import kotlinx.fuzz.KFuzzConfig
 import kotlinx.fuzz.KFuzzEngine
-import java.lang.invoke.MethodHandles
 import java.lang.reflect.Method
-import java.util.concurrent.atomic.AtomicReference
 import kotlin.io.path.ExperimentalPathApi
-import kotlin.io.path.createDirectories
-import kotlin.reflect.jvm.javaMethod
 
 @Suppress("unused")
 class JazzerEngine(private val config: KFuzzConfig) : KFuzzEngine {
     private val jazzerConfig = JazzerConfig.fromSystemProperties()
 
-    override fun initialise() {
-        Log.fixOutErr(System.out, System.err)
+    override fun initialise(): Unit = Unit
 
-        Opt.hooks.setIfDefault(config.hooks)
-        Opt.instrumentationIncludes.setIfDefault(config.instrument)
-        Opt.customHookIncludes.setIfDefault(config.instrument)
-        Opt.customHookExcludes.setIfDefault(config.customHookExcludes)
-
-        AgentInstaller.install(Opt.hooks.get())
-
-        FuzzTargetHolder.fuzzTarget = FuzzTargetHolder.FuzzTarget(
-            JazzerTarget::fuzzTargetOne.javaMethod,
-            LifecycleMethodsInvoker.noop(JazzerTarget),
-        )
-    }
 
     @OptIn(ExperimentalPathApi::class)
     override fun runTarget(instance: Any, method: Method): Throwable? {
-        val libFuzzerArgs = mutableListOf("fake_argv0")
-        val corpusDir = config.workDir.resolve("corpus").resolve(method.name)
-        corpusDir.createDirectories()
+        // spawn subprocess, redirect output to log and err files
 
-        libFuzzerArgs += corpusDir.toString()
-        libFuzzerArgs += "-max_total_time=${config.maxSingleTargetFuzzTime.inWholeSeconds}"
-        libFuzzerArgs += "-rss_limit_mb=${jazzerConfig.libFuzzerRssLimit}"
+        val classpath = System.getProperty("java.class.path")
+        val mainClass = "kotlinx.fuzz.jazzer.Launcher"
+        val javaCommand = System.getProperty("java.home") + "/bin/java"
 
-        val atomicFinding = AtomicReference<Throwable>()
-        FuzzTargetRunner.registerFatalFindingHandlerForJUnit { finding ->
-            atomicFinding.set(finding)
+        // to pass `config`
+        val properties = config.toPropertiesMap().map { (key, value) -> "-D$key=$value"}.toTypedArray()
+
+        val pb = ProcessBuilder(
+            javaCommand,
+            "-classpath", classpath,
+            *properties,
+            mainClass,
+            method.declaringClass.name, method.name,
+        )
+
+        pb.inheritIO()
+        val res = pb.start().waitFor()
+        if (res == 0) {
+            return null
         }
-
-        JazzerTarget.reset(MethodHandles.lookup().unreflect(method), instance)
-        FuzzTargetRunner.startLibFuzzer(libFuzzerArgs)
-
-        // corpusDir.deleteRecursively()
-
-        return atomicFinding.get()
+        // TODO: read real exception
+        return Throwable("Jazzer subprocess returned with code $res")
     }
 }
---
+++ kotlinx.fuzz.jazzer/src/main/kotlin/kotlinx/fuzz/jazzer/Launcher.kt
@@ -0,0 +1,101 @@
+package kotlinx.fuzz.jazzer
+
+import com.code_intelligence.jazzer.agent.AgentInstaller
+import com.code_intelligence.jazzer.driver.FuzzTargetHolder
+import com.code_intelligence.jazzer.driver.FuzzTargetRunner
+import com.code_intelligence.jazzer.driver.LifecycleMethodsInvoker
+import com.code_intelligence.jazzer.driver.Opt
+import com.code_intelligence.jazzer.utils.Log
+import kotlinx.fuzz.KFuzzConfig
+import java.io.PrintStream
+import java.lang.invoke.MethodHandles
+import java.lang.reflect.Method
+import java.nio.file.Path
+import java.util.concurrent.atomic.AtomicReference
+import kotlin.io.path.ExperimentalPathApi
+import kotlin.io.path.createDirectories
+import kotlin.io.path.outputStream
+import kotlin.reflect.full.memberFunctions
+import kotlin.reflect.full.primaryConstructor
+import kotlin.reflect.jvm.javaMethod
+import kotlin.system.exitProcess
+
+object Launcher {
+    private val config = KFuzzConfig.fromSystemProperties()
+    private val jazzerConfig = JazzerConfig.fromSystemProperties()
+
+    val corpusDir: Path = config.workDir.resolve("corpus")
+    val logsDir: Path = config.workDir.resolve("logs")
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        if (args.size != 2) {
+            println("Usage: <full.class.Name> <methodName>")
+            exitProcess(1)
+        }
+        val className = args[0]
+        val methodName = args[1]
+        println("Running $className::$methodName")
+
+        val targetClass = Class.forName(className).kotlin
+        val targetMethod = targetClass.memberFunctions.single { it.name == methodName }.javaMethod!!
+        val instance = targetClass.objectInstance ?: targetClass.primaryConstructor!!.call()
+
+        initJazzer()
+
+
+        val error = runTarget(instance, targetMethod)
+        if (error != null) {
+            System.err.println(error)
+            exitProcess(1)
+        }
+        exitProcess(0)
+    }
+
+
+    @OptIn(ExperimentalPathApi::class)
+    fun runTarget(instance: Any, method: Method): Throwable? {
+        val libFuzzerArgs = mutableListOf("fake_argv0")
+        val currentCorpus = corpusDir.resolve(method.name)
+        currentCorpus.createDirectories()
+
+        libFuzzerArgs += currentCorpus.toString()
+        libFuzzerArgs += "-max_total_time=${config.maxSingleTargetFuzzTime.inWholeSeconds}"
+        libFuzzerArgs += "-rss_limit_mb=${jazzerConfig.libFuzzerRssLimit}"
+
+        val atomicFinding = AtomicReference<Throwable>()
+        FuzzTargetRunner.registerFatalFindingHandlerForJUnit { finding ->
+            atomicFinding.set(finding)
+        }
+
+        JazzerTarget.reset(MethodHandles.lookup().unreflect(method), instance)
+        FuzzTargetRunner.startLibFuzzer(libFuzzerArgs)
+
+        // corpusDir.deleteRecursively()
+
+        return atomicFinding.get()
+    }
+
+    fun initJazzer() {
+        logsDir.createDirectories()
+        Log.fixOutErr(
+            PrintStream(logsDir.resolve("fuzz.log").outputStream()),
+            PrintStream(logsDir.resolve("fuzz.err").outputStream())
+        )
+
+        Log.fixOutErr(System.out, System.err)
+
+        Opt.hooks.setIfDefault(config.hooks)
+        Opt.instrumentationIncludes.setIfDefault(config.instrument)
+        Opt.customHookIncludes.setIfDefault(config.instrument)
+        Opt.customHookExcludes.setIfDefault(config.customHookExcludes)
+
+        AgentInstaller.install(Opt.hooks.get())
+
+        FuzzTargetHolder.fuzzTarget = FuzzTargetHolder.FuzzTarget(
+            JazzerTarget::fuzzTargetOne.javaMethod,
+            LifecycleMethodsInvoker.noop(JazzerTarget),
+        )
+
+    }
+}
