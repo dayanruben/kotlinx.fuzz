e55728d4a71b4a7cfec133ffca0fb286373dcbae
FerrumBrain <lemur1416@gmail.com>
2025-02-05 15:02:17 +0100

    change tests

--- kotlinx.fuzz.gradle/src/test/kotlin/kotlinx.fuzz.gradle.junit.test/EngineTest.kt
+++ kotlinx.fuzz.gradle/src/test/kotlin/kotlinx.fuzz.gradle.junit.test/EngineTest.kt
@@ -15,7 +15,7 @@ import org.junit.platform.testkit.engine.EngineTestKit
 object EngineTest {
     object SimpleFuzzTest {
         @KFuzzTest
-        fun `failure test`(data: KFuzzer) {
+        fun `one failure test`(data: KFuzzer) {
             if (data.boolean()) {
                 error("Expected failure")
             }
@@ -35,9 +35,11 @@ object EngineTest {
 
         @KFuzzTest
         fun `failure test2`(data: KFuzzer) {
-            if (data.int() in 16..24) {
+            if (data.int() in 21..25) {
                 val file = File("fuzz-test.txt")
                 file.readText()
+            } else if (data.int() in 26..30) {
+                throw IllegalArgumentException("Expected failure")
             }
         }
 
@@ -46,13 +48,19 @@ object EngineTest {
             if (data.int() in 5..15) {
                 val b = System.getProperty("abibaboba")
                 System.setProperty("abacaba", b!!)
+            } else if (data.int() in 16..20) {
+                val file = File("fuzz-test.txt")
+                file.readText()
             }
         }
 
         @KFuzzTest
         fun `failure test4`(data: KFuzzer) {
-            if (data.int() in 25..215) {
+            if (data.int() in 30..215) {
                 throw IllegalArgumentException("Expected failure")
+            } else if (data.int() in 216..314) {
+                val b = System.getProperty("abibaboba")
+                System.setProperty("abacaba", b!!)
             }
         }
     }
@@ -63,13 +71,14 @@ object EngineTest {
             maxSingleTargetFuzzTime = 5.seconds
             instrument = listOf("kotlinx.fuzz.test.**")
             workDir = kotlin.io.path.createTempDirectory("fuzz-test")
+            keepGoing = 2
         }
     }
 
     @Test
-    fun `two pass four fail`() {
-        val successTests = 2L
-        val failedTests = 4L
+    fun `three pass three fail`() {
+        val successTests = 3L
+        val failedTests = 3L
         val startedTests = successTests + failedTests
 
         EngineTestKit
