a4abf6377bcc943236b8dd0a9a41f8ae9a840e02
FerrumBrain <lemur1416@gmail.com>
2025-02-07 13:47:53 +0100

    fuzzing debug

--- kotlinx.fuzz.jazzer/src/main/kotlin/kotlinx/fuzz/jazzer/JazzerEngine.kt
+++ kotlinx.fuzz.jazzer/src/main/kotlin/kotlinx/fuzz/jazzer/JazzerEngine.kt
@@ -10,7 +10,9 @@ import kotlin.io.path.*
 import kotlinx.fuzz.KFuzzConfig
 import kotlinx.fuzz.KFuzzEngine
 import kotlinx.fuzz.log.LoggerFacade
+import kotlinx.fuzz.log.debug
 import kotlinx.fuzz.log.error
+import java.lang.management.ManagementFactory
 
 internal val Method.fullName: String
     get() = "${this.declaringClass.name}.${this.name}"
@@ -29,22 +31,39 @@ class JazzerEngine(private val config: KFuzzConfig) : KFuzzEngine {
     private val log = LoggerFacade.getLogger<JazzerEngine>()
     private val jazzerConfig = JazzerConfig.fromSystemProperties()
 
+    companion object {
+        private const val PORT = "5005"
+        private const val DEBUG_SETUP_STRING =
+            "-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=localhost:$PORT"
+    }
+
     override fun initialise() {
         config.corpusDir.createDirectories()
         config.logsDir.createDirectories()
         config.exceptionsDir.createDirectories()
     }
 
-    @OptIn(ExperimentalPathApi::class)
+    private fun isDebugMode(): Boolean {
+        return ManagementFactory.getRuntimeMXBean().inputArguments.any { it.contains("-agentlib:jdwp") }
+    }
+
     override fun runTarget(instance: Any, method: Method): Throwable? {
         // spawn subprocess, redirect output to log and err files
         val classpath = System.getProperty("java.class.path")
         val javaCommand = System.getProperty("java.home") + "/bin/java"
         val properties = System.getProperties().map { (property, value) -> "-D$property=$value" }
 
+        val debugOptions = if (isDebugMode()) {
+            log.debug { "Connect Remote JVM Debugger (port $PORT) or attach to process with name \"kotlinx.fuzz.jazzer.JazzerLauncher\"" }
+            listOf(DEBUG_SETUP_STRING)
+        } else {
+            emptyList()
+        }
+
         val exitCode = ProcessBuilder(
             javaCommand,
             "-classpath", classpath,
+            *debugOptions.toTypedArray(),
             *properties.toTypedArray(),
             JazzerLauncher::class.qualifiedName!!,
             method.declaringClass.name, method.name,
