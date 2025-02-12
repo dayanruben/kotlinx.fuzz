668d6378c0e6868a1f9d2898892a68f25bbb43d6
FerrumBrain <lemur1416@gmail.com>
2025-02-12 17:33:25 +0100

    fix

--- kotlinx.fuzz.gradle/src/test/kotlin/kotlinx.fuzz.gradle.junit.test/EngineTest.kt
+++ kotlinx.fuzz.gradle/src/test/kotlin/kotlinx.fuzz.gradle.junit.test/EngineTest.kt
@@ -90,7 +90,7 @@ object EngineTest {
     @Test
     fun `passes and failures`() {
         val successTests = 3L
-        val failedTests = 3L
+        val failedTests = 4L
         val startedTests = successTests + failedTests
 
         EngineTestKit
