dbc36f742ff40aca033dd96852dd11fd7b34ac09
Ilia Malakhov <ilya.malakhov4@gmail.com>
2025-03-19 15:20:38 +0100

    setup for kotlinx. - serialization - cli - html - io

--- kotlinx.fuzz.examples/kotlinx.html/build.gradle.kts
+++ kotlinx.fuzz.examples/kotlinx.html/build.gradle.kts
@@ -1,14 +1,34 @@
+import kotlinx.fuzz.config.CoverageReportType
+import kotlin.time.Duration.Companion.seconds
+
 plugins {
     id("kotlinx.fuzz.example-module")
+    id("kotlinx.fuzz.gradle")
 }
 
 
 dependencies {
     testImplementation(kotlin("reflect"))
-    implementation(libs.jazzer.api)
-    implementation(libs.jazzer.junit)
+    implementation(libs.plan.jazzer.api)
+    implementation(libs.plan.jazzer.junit)
     implementation(libs.reflections)
     implementation(libs.jsoup)
     implementation(libs.kotlinx.html)
     implementation(libs.kotlinx.html.jvm)
+
+    testRuntimeOnly("org.jetbrains:kotlinx.fuzz.jazzer")
+
+}
+
+fuzzConfig {
+    instrument = listOf("kotlinx.html.**")
+    maxFuzzTimePerTarget = 10.seconds // TODO
+    supportJazzerTargets = true
+    logLevel = kotlinx.fuzz.config.LogLevel.DEBUG
+//    detailedLogging = true
+    coverage {
+        reportTypes = setOf(CoverageReportType.HTML, CoverageReportType.CSV)
+        val immutable = libs.kotlinx.html.jvm.get()
+        includeDependencies = setOf("${immutable.group}:${immutable.name}-jvm")
+    }
 }
--- kotlinx.fuzz.examples/kotlinx.io/build.gradle.kts
+++ kotlinx.fuzz.examples/kotlinx.io/build.gradle.kts
@@ -1,11 +1,30 @@
+import kotlinx.fuzz.config.CoverageReportType
+import kotlin.time.Duration.Companion.seconds
+
 plugins {
     id("kotlinx.fuzz.example-module")
+    id("kotlinx.fuzz.gradle")
 }
 
 dependencies {
     implementation(kotlin("reflect"))
-    implementation(libs.jazzer.api)
-    implementation(libs.jazzer.junit)
+    implementation(libs.plan.jazzer.api)
+    implementation(libs.plan.jazzer.junit)
     implementation(libs.reflections)
     implementation(libs.kotlinx.io.core)
+
+    testRuntimeOnly("org.jetbrains:kotlinx.fuzz.jazzer")
+}
+
+fuzzConfig {
+    instrument = listOf("kotlinx.io.**")
+    maxFuzzTimePerTarget = 10.seconds // TODO
+    supportJazzerTargets = true
+    logLevel = kotlinx.fuzz.config.LogLevel.DEBUG
+//    detailedLogging = true
+    coverage {
+        reportTypes = setOf(CoverageReportType.HTML, CoverageReportType.CSV)
+        val immutable = libs.kotlinx.io.core.get()
+        includeDependencies = setOf("${immutable.group}:${immutable.name}-jvm")
+    }
 }
